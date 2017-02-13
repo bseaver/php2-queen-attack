fc8295d9211e2d3c086e58fcf92e496d49e628f0
Benjamin T. Seaver and Sarah Leahy <sarah@>
2017-02-13 15:11:48 -0800

    pass second test case

--- src/Queen.php
+++ src/Queen.php
@@ -3,19 +3,18 @@
     {
         private $coordinates;
 
-        function __construct()
-        {
-
-        }
-
         function setCoordinates($new_coordinates)
         {
             $this->coordinates = $new_coordinates;
         }
 
-        function canAttack()
+        function canAttack($opposing_coordinates)
         {
-
+            $output = false;
+            if ($opposing_coordinates[0] == $this->coordinates[0]) {
+                return true;
+            }
+            return $output;
         }
     }
  ?>
